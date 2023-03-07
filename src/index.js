3cf7297fa176eefa3e8b1d98ea167c5cb2ca4329
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-03-07 19:39:03 +0530

    Added RabbitMQ initial config

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const app = express();
 const { PORT } = require("./config/serverConfig");
 const jobs = require("./utils/job");
 const apiRoutes = require("./routes/index");
+const { createChannel } = require("./utils/message-queue");
 
-const setupAndStartServer = (req, res) => {
+const setupAndStartServer = async (req, res) => {
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use('/api', apiRoutes);
+
+    // const channel = await createChannel();
 
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
